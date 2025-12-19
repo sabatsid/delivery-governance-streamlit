1160f023eadb2e457f3928adc530ca15d01d0e06
Sidharth Sabat <46293867+sabatsid@users.noreply.github.com>
2025-12-19 15:19:55 +0530

    Program View Tab 01

--- app.py
+++ app.py
@@ -199,9 +199,9 @@ if st.session_state.get("logged_in"):
 # -------------------------
 def program_manager_page():
    # for reset
-    for key in ["rag_filter", "sla_filter", "lifecycle_filter"]:
-        if key not in st.session_state:
-        st.session_state[key] = []
+for key in ["rag_filter", "sla_filter", "lifecycle_filter"]:
+    if key not in st.session_state:
+    st.session_state[key] = []
     # for reset
     
     st.title("🧭 Program Master View")
