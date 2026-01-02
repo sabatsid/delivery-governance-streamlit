7ab95d62b761462940de6b66efd8f212243f8709
Sidharth Sabat <46293867+sabatsid@users.noreply.github.com>
2026-01-02 13:06:57 +0530

    Update customer_view demo 02

--- views/customer_view.py
+++ views/customer_view.py
@@ -1,4 +1,8 @@
 import streamlit as st
+import pandas as pd
 
 def customer_view(data):
-    st.title("Customer View OK")
+    st.title("📦 Track Your Order")
+    st.caption("Real-time visibility into your order and support")
+
+    st.dataframe(data["orders"].head())
