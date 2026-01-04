d520bf8a49e72d382f6f5e2c69e4134a9a2dfabc
Sidharth Sabat <46293867+sabatsid@users.noreply.github.com>
2026-01-04 23:32:02 +0530

    Remove display of last ticket after raising a new one
    Removed the display of the last customer ticket after raising a new ticket.

--- views/customer_view.py
+++ views/customer_view.py
@@ -128,8 +128,6 @@ def customer_view(data):
             "Our team will contact you shortly."
         )
 
-        st.write(st.session_state.customer_tickets[-1])
-
     # -------------------------
     # VIEW PREVIOUS TICKETS
     # -------------------------
